d9aea415d3aad9d30140148b0257cc316832ed44
Diya Bhargava <117004051+diyabhargava27@users.noreply.github.com>
2026-05-26 21:26:05 +0530

    Update axiosInstance.js

--- frontend/budgit-fit/src/utils/axiosInstance.js
+++ frontend/budgit-fit/src/utils/axiosInstance.js
@@ -1,6 +1,50 @@
 // utils/axiosInstance.js
 import axios from "axios";
-export const BASE_URL = 'https://budget-fit-backend-9e74.onrender.com/api/v1';  // Backend URL
+export const BASE_URL = 'require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
+const incomeRoutes = require("./routes/IncomeRoutes");
+const expenseRoutes = require("./routes/expenseRoutes");
+const dashboardRoutes = require("./routes/dashboardRoutes");
+
+const app = express();
+app.use(express.json());
+
+// CORS middleware with a specific origin
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://budget-fit-frontend-4l0v.onrender.com"
+];
+
+app.use(cors({
+  origin: function(origin, callback) {
+    if (!origin) return callback(null, true);  // allow REST clients, Postman etc.
+    if (allowedOrigins.indexOf(origin) === -1) {
+      return callback(new Error("Not allowed by CORS"), false);
+    }
+    callback(null, true);
+  },
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+}));
+
+
+connectDB();
+
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/income", incomeRoutes);
+app.use("/api/v1/expense", expenseRoutes);
+app.use("/api/v1/dashboard", dashboardRoutes);
+
+// Server upload folder
+app.use('/uploads', express.static('uploads'));
+
+
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));/api/v1';  // Backend URL
 const axiosInstance = axios.create({
   baseURL: BASE_URL,
   timeout: 10000,
