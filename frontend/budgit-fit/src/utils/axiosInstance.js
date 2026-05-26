583489034e41e9f462539252513709c91feefb71
Diya Bhargava <117004051+diyabhargava27@users.noreply.github.com>
2026-05-26 22:35:14 +0530

    Update axiosInstance.js

--- frontend/budgit-fit/src/utils/axiosInstance.js
+++ frontend/budgit-fit/src/utils/axiosInstance.js
@@ -43,7 +43,7 @@ app.use("/api/v1/dashboard", dashboardRoutes);
 app.use('/uploads', express.static('uploads'));
 
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));/api/v1';  // Backend URL
 const axiosInstance = axios.create({
   baseURL: BASE_URL,
